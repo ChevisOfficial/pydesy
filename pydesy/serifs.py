6ac3af983bc3fb24749561a0c17ed1470d0d15dc
ChevisOfficial <maxim.shmakov@outlook.com>
2021-12-02 15:25:04 +0300

    fix linear serif

--- pydesy/serifs.py
+++ pydesy/serifs.py
@@ -51,24 +51,24 @@ class PolarSerif:
 
 
 class LinearSerif:
-    def __init__(self, points: list[Point]):
-        self.points = points
+    def __init__(self, pointGroups: list[list[Point]]):
+        self.pointGroups = pointGroups
 
-    def set_side_lengths(self, sideLengths: list[float]):
-        self.sideLengths = sideLengths
+    def set_side_lengths(self, sideLengthGroups: list[list[float]]):
+        self.sideLengthGroups = sideLengthGroups
 
     def calculate_serif(self) -> Point:
         self.calculatedPoints = list()
         self.gammas = list()
-        for i in range(1, len(self.points)):
-            basis, directionAngle = self.points[i-1].igt(self.points[i])
-            gamma, sigma, betta = three_sides(basis, self.sideLengths[i-1], self.sideLengths[i])
+        for i, points in enumerate(self.pointGroups):
+            basis, directionAngle = points[0].igt(points[1])
+            gamma, sigma, betta = three_sides(basis, self.sideLengthGroups[i][0], self.sideLengthGroups[i][1])
             correctedDirectionAngle = revs(directionAngle - betta)
-            self.calculatedPoints.append(self.points[i-1].dgt(self.sideLengths[i-1], correctedDirectionAngle))
+            self.calculatedPoints.append(points[0].dgt(self.sideLengthGroups[i][0], correctedDirectionAngle))
             self.gammas.append(gamma)
-            if i == len(self.points):
+            if i == len(self.pointGroups):
                 correctedDirectionAngle = revs(directionAngle + sigma)
-                self.calculatedPoints.append(self.points[i].dgt(self.sideLengths[i], correctedDirectionAngle))
+                self.calculatedPoints.append(points[1].dgt(self.sideLengthGroups[i][1], correctedDirectionAngle))
 
         self.calculatedPoint = Point()
         for point in self.calculatedPoints:
@@ -78,18 +78,21 @@ class LinearSerif:
     def calculate_accuracy_score(self, lenghtError: float) -> list[float]:
         self.errors = list()
         for gamma in self.gammas:
-            self.errors.append(lenghtError * sqrt(2) / sin(gamma))
-        return sum(self.errors) / len(self.errors)
+            self.errors.append((lenghtError * sqrt(2) / sin(gamma))**2)
+        return sqrt(sum(self.errors)) / len(self.errors)
 
     def draw_serif(self):
         plt.figure("Linear serif")
-        plt.text(self.calculatedPoint.y, self.calculatedPoint.x, "?")
-        for i, point in enumerate(self.points):
-            plt.plot([point.y, self.calculatedPoint.y], [point.x, self.calculatedPoint.x], linestyle="--", color="blue", marker=".")
-            plt.text(point.y, point.x, f"{i}")
-        for i in range(1, len(self.points)):
-            plt.plot([self.points[i-1].y, self.points[i].y], [self.points[i-1].x, self.points[i].x], color="red", marker="^")
+        plt.text(self.calculatedPoint.y, self.calculatedPoint.x, "P")
+        for i, points in enumerate(self.pointGroups):
+            plt.plot([points[0].y, self.calculatedPoint.y], [points[0].x, self.calculatedPoint.x], linestyle="--", color="blue", marker=".")
+            plt.text(points[0].y, points[0].x, f"{i}")
+            if i == len(self.pointGroups) - 1:
+                plt.plot([points[1].y, self.calculatedPoint.y], [points[1].x, self.calculatedPoint.x], linestyle="--", color="blue", marker=".")
+                plt.text(points[1].y, points[1].x, f"{i}")
+            plt.plot([points[0].y, points[1].y], [points[0].x, points[1].x], color="red", marker="^")
         plt.xlabel("y")
         plt.ylabel("x")
         plt.title("Linear serif")
         plt.show()
+
